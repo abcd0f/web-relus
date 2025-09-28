4544bc119d7d4fac164cc0a679d9232f9172e7c3
Sun <wanglt-123@qq.com>
2025-09-28 14:16:15 +0800

    docs(test): 添加组件使用方法注释
    在test.vue组件的script标签中添加了详细的使用说明注释，
    解释了如何在md文件中引入并渲染该组件示例，
    便于其他开发者了解和使用此测试组件。

--- docs/examples/test/test.vue
+++ docs/examples/test/test.vue
@@ -2,6 +2,16 @@
   <div>我是测试的代码</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/**
+ * 使用方法
+ * 在md文件中引入路径 test/test
+ * 会自动渲染组件示例
+ *
+ * :::demo
+ * test/test
+ * :::
+ */
+</script>
 
 <style scoped></style>
